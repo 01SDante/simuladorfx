338d2df562a935fce452f003681252c132725fb8
dante <sack_dante@hotmail.com>
2019-11-10 19:09:29 -0300

    Processes complete

---
+++ src/application/algorithms/FCFS.java
@@ -0,0 +1,13 @@
+package application.algorithms;
+
+import application.model.Particion;
+import application.model.Proceso;
+import javafx.collections.ObservableList;
+
+public class FCFS {
+	
+	public static void ejecutar(ObservableList<Particion> particiones, ObservableList<Proceso> procesos) {
+		System.out.println("FCFS");
+	}
+
+}
